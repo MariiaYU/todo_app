cde50f6b6151df24026c2c94d545cd41e16fb061
mariia <mariia.yukhym@gmail.com>
2024-09-17 15:08:13 -0400

    Added "add" functionality updated todo list with new items

--- TodoProject/todo_list_gui.py
+++ TodoProject/todo_list_gui.py
@@ -2,12 +2,25 @@ import FreeSimpleGUI as sg
 import functions
 
 label = sg.Text("Enter to-do: ")
-input_box = sg.InputText("Enter to-do")
+input_box = sg.InputText(key="todo")
 button = sg.Button("Add")
 
 layout = [[label],
           [input_box, button]]
 
-window = sg.Window("To-do List", layout=layout)
-window.read()
+window = sg.Window("To-do List", layout=layout, font=("Helvetica", 20))
+
+
+while True:
+    event, values = window.read()
+
+    match event:
+        case "Add":
+            todo = values["todo"]
+            todos = functions.get_todos()
+            todos.append(todo + "\n")
+            functions.update_file(todos)
+        case sg.WIN_CLOSED():
+            break
+
 window.close()
