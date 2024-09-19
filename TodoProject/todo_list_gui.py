8adec9309e6c33b80bb9041cd34ab845169e5915
mariia <mariia.yukhym@gmail.com>
2024-09-19 12:32:22 -0400

    Added "edit" functionality

--- TodoProject/todo_list_gui.py
+++ TodoProject/todo_list_gui.py
@@ -3,24 +3,45 @@ import functions
 
 label = sg.Text("Enter to-do: ")
 input_box = sg.InputText(key="todo")
-button = sg.Button("Add")
+button_add = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[45, 10])
+button_edit = sg.Button("Edit")
+
 
 layout = [[label],
-          [input_box, button]]
+          [input_box, button_add],
+          [list_box, button_edit]
+          ]
 
 window = sg.Window("To-do List", layout=layout, font=("Helvetica", 20))
 
-
 while True:
     event, values = window.read()
-
+    print(values)
     match event:
         case "Add":
             todo = values["todo"]
             todos = functions.get_todos()
             todos.append(todo + "\n")
             functions.update_file(todos)
-        case sg.WIN_CLOSED():
+
+            window["todos"].update(values=todos)
+
+        case "todos":
+            window["todo"].update(value=values["todos"][0])
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo = values["todo"]
+
+            todos = functions.get_todos()
+            number = todos.index(todo_to_edit)
+
+            todos[number] = new_todo + "\n"
+            functions.update_file(todos)
+
+            window["todos"].update(values=todos)
+
+        case sg.WIN_CLOSED:
             break
 
 window.close()
