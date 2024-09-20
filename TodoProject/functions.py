5c5e4955891adc201725927be683e3f37207e297
mariia <mariia.yukhym@gmail.com>
2024-09-20 15:35:29 -0400

    Added "Complete" and "Exit" functionality Fixed the bag with \n

--- TodoProject/functions.py
+++ TodoProject/functions.py
@@ -1,3 +1,5 @@
+import FreeSimpleGUI
+
 FILEPATH = "todos.txt"
 
 
--- TodoProject/todo_list_gui.py
+++ TodoProject/todo_list_gui.py
@@ -6,11 +6,14 @@ input_box = sg.InputText(key="todo")
 button_add = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[45, 10])
 button_edit = sg.Button("Edit")
-
+button_complete = sg.Button("Complete")
+button_exit = sg.Button("Exit")
+message = sg.Text()
 
 layout = [[label],
           [input_box, button_add],
-          [list_box, button_edit]
+          [list_box, button_edit, button_complete],
+          [button_exit]
           ]
 
 window = sg.Window("To-do List", layout=layout, font=("Helvetica", 20))
@@ -20,18 +23,21 @@ while True:
     print(values)
     match event:
         case "Add":
-            todo = values["todo"]
+            todo = values["todo"].capitalize()
             todos = functions.get_todos()
             todos.append(todo + "\n")
             functions.update_file(todos)
 
             window["todos"].update(values=todos)
+            window["todo"].update(value="")
 
         case "todos":
             window["todo"].update(value=values["todos"][0])
+
         case "Edit":
             todo_to_edit = values["todos"][0]
-            new_todo = values["todo"]
+            todo_to_edit = todo_to_edit
+            new_todo = values["todo"].replace("\n", "")
 
             todos = functions.get_todos()
             number = todos.index(todo_to_edit)
@@ -40,8 +46,21 @@ while True:
             functions.update_file(todos)
 
             window["todos"].update(values=todos)
+            window["todo"].update(value="")
 
-        case sg.WIN_CLOSED:
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+
+            todos = functions.get_todos()
+            number = todos.index(todo_to_complete)
+
+            todos.remove(todos[number])
+            functions.update_file(todos)
+
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+
+        case sg.WIN_CLOSED | "Exit":
             break
 
 window.close()
