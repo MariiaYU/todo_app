82ea86866f502f9387495d8b391d882c9281a64e
mariia <mariia.yukhym@gmail.com>
2024-09-24 13:15:49 -0400

    added add and remove todo functionality

--- TodoProject/todo_list_web.py
+++ TodoProject/todo_list_web.py
@@ -1,11 +1,27 @@
 import streamlit as st
 import functions
 
-st.title("My Todo App")
 
 todos = functions.get_todos()
-for todo in todos:
-    st.checkbox(todo)
 
-st.text_input(label="Enter a todo:", placeholder="Add a new todo...")
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.update_file(todos)
+
+st.title("My Todo App")
+
+for index, item in enumerate(todos):
+    checkbox = st.checkbox(item, key=item)
+    if checkbox:
+        todos.pop(index)
+        functions.update_file(todos)
+        del st.session_state[item]
+        st.rerun()
+
+
+st.text_input(label="Enter a todo:", placeholder="Add a new todo...",
+              on_change=add_todo, key="new_todo")
+
 
